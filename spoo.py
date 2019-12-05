803c2845c8e2fbfb9fa893edadbd01617ec4b31a
ensyvz14 <ensyvz14@gmail.com>
2019-12-06 02:07:20 +0300

    Exceptions handled, authentication problem solved.

--- spoo.py
+++ spoo.py
@@ -6,35 +6,36 @@ import json
 import time
 import dbOperation as db
 import threading
-
+import datetime
 
 token = "" 
 
-def msToMinute(ms):
+def msToMinute(ms): # Changes milliseconds to minutes
     second = ms/1000
     minute = second // 60
     second = second%60/100
     time=minute+second
     return time
 
-def auth():
+def auth(): # Spotify token expires after 1 hour so this subprocess will refresh token every 1hour5seconds
     global token
     while True:
         cid = '1613417d61fe44e2a072fbd78f0c684d'
         csec = 'bdef638e4f564c7e9eee361a4440ccb0'
-        redir='http://localhost'
+        redir='http://localhost/'
 
         username = "x5ewxw339c9q44th6z4cpopeu"
         scope= "user-read-currently-playing user-read-playback-state"
 
         token = util.prompt_for_user_token(username, scope,cid,csec,redir)
-        time.sleep(3500)
+        print(token+" | "+datetime.datetime.now().strftime("%H:%M:%S"))
+        time.sleep(3605)
 
 def main():
     global token
     cid = '1613417d61fe44e2a072fbd78f0c684d'
     csec = 'bdef638e4f564c7e9eee361a4440ccb0'
-    redir='http://localhost'
+    redir='http://localhost/'
 
     username = "x5ewxw339c9q44th6z4cpopeu"
     scope= "user-read-currently-playing user-read-playback-state"
@@ -46,18 +47,22 @@ def main():
     nowArtistName=""
     prog = 0
     count = 0
-    authOp = threading.Thread(target=auth) 
+    authOp = threading.Thread(target=auth) # Thread for every hour authentication
     authOp.start()
     while True:
         prevList = nowSongName
         prevProg = prog
 
-        result = sp.current_user_playing_track()
-
-        if result:
-            prog = msToMinute(int(result['progress_ms']))
-
+        try: # When token expires there will be an error about it 
+            result = sp.current_user_playing_track()
             resultPb = sp.current_playback()
+        except: # Catching that error and trying to get a new object with new token (if token is not new then there will be an another exception)
+            try:
+                sp = spotipy.Spotify(auth=token)
+            except:
+                continue
+        if result and resultPb:
+            prog = msToMinute(int(result['progress_ms']))
 
             nowSongName = resultPb['item']['name']
             nowArtistName = resultPb['item']['artists'][0]['name']
@@ -67,10 +72,9 @@ def main():
             elif prog != prevProg:
                 count = 1
 
-            print(count)
-
-            if count == 150:
+            if count == 120:
                 db.incCounter(nowSongName,nowArtistName)
-            time.sleep(0.5)
+            
+            time.sleep(0.3)
         else:
             continue
--- zer.py
+++ zer.py
@@ -6,14 +6,19 @@ import dbOperation as db
 import time
 
 def download(outputFolder):
+    token = "884d9cf88f80e652ea8d78b64ed7cd1a60e7c7ecf2ee2c74811837ece0291b477e9c29e8e638b6a5c5b37cab0bf13b726cfb27fc09e55ecb74f5785dccee18c96a84220527d166b527f830c857a7b5128f4e3d194b78d8cb63bd6467c7d8c4e4"
+    downloa = Login(token)
+    
     quality = "FLAC"
     output = outputFolder
     recursive_quality = False
     recursive_download = False
-    zips=False
     not_gui=False
+    song = ""
+    artist = ""
 
     downQueue = db.getDownloadQueue()
+
     for songs in downQueue:
         song = songs[0]
         artist = songs[1]
@@ -25,13 +30,7 @@ def download(outputFolder):
         )
         db.downloaded(song,artist)
 
-def main(outputFolder):
-    token = "884d9cf88f80e652ea8d78b64ed7cd1a60e7c7ecf2ee2c74811837ece0291b477e9c29e8e638b6a5c5b37cab0bf13b726cfb27fc09e55ecb74f5785dccee18c96a84220527d166b527f830c857a7b5128f4e3d194b78d8cb63bd6467c7d8c4e4"
-    downloa = Login(token)
-
-    song = ""
-    artist = ""
-
+def main(outputFolder): 
     while True:
         download(outputFolder)
         time.sleep(3600)
